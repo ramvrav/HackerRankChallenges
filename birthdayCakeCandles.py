ba3eb72bac58d7db0625f05732faf5be1b903c7b
ramvrav <33099104+ramvrav@users.noreply.github.com>
2023-08-28 13:03:57 +0530

    Create birthdayCakeCandles.py

---
+++ birthdayCakeCandles.py
@@ -0,0 +1,62 @@
+'''
+You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+
+Example
+
+
+The maximum height candles are  units high. There are  of them, so return .
+
+Function Description
+
+Complete the function birthdayCakeCandles in the editor below.
+
+birthdayCakeCandles has the following parameter(s):
+
+int candles[n]: the candle heights
+Returns
+
+int: the number of candles that are tallest
+Input Format
+
+The first line contains a single integer, , the size of .
+The second line contains  space-separated integers, where each integer  describes the height of .
+
+Constraints
+
+Sample Input 0
+
+4
+3 2 1 3
+Sample Output 0
+
+2
+Explanation 0
+
+Candle heights are . The tallest candles are  units, and there are  of them.
+'''
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthdayCakeCandles function below.
+def birthdayCakeCandles(ar):
+    tall_candle_type=max(ar)
+    tall_candles=ar.count(tall_candle_type)
+    return(tall_candles)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
