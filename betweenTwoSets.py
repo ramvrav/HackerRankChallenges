8486cd0919b071e50e5cacb8e8dd9bef3d5523f1
ramvrav <33099104+ramvrav@users.noreply.github.com>
2023-09-01 16:48:07 +0530

    Update betweenTwoSets.py

--- betweenTwoSets.py
+++ betweenTwoSets.py
@@ -1,16 +1,13 @@
 '''
 There will be two arrays of integers. Determine all integers that satisfy the following two conditions:
-
 The elements of the first array are all factors of the integer being considered
 The integer being considered is a factor of all elements of the second array
 These numbers are referred to as being between the two arrays. Determine how many such numbers exist.
-
+https://www.hackerrank.com/challenges/between-two-sets/problem
 '''
 #!/bin/python3
-
 import os
 import sys
-
 #
 # Complete the getTotalX function below.
 #
@@ -22,24 +19,15 @@ def getTotalX(a, b):
     for values in range(1,int(nmin/nmax)+1):
         if(sum((values*nmax)%n for n in a)+sum(n%(values*nmax) for n in b))==0:
             count+=1
-    return(count)
-            
+    return(count)            
 
 if __name__ == '__main__':
     f = open(os.environ['OUTPUT_PATH'], 'w')
-
     nm = input().split()
-
     n = int(nm[0])
-
     m = int(nm[1])
-
     a = list(map(int, input().rstrip().split()))
-
     b = list(map(int, input().rstrip().split()))
-
     total = getTotalX(a, b)
-
     f.write(str(total) + '\n')
-
     f.close()
