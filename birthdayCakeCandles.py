f346119e41ebb8d86559a452340312ddbb51e98b
ramvrav <33099104+ramvrav@users.noreply.github.com>
2023-09-01 16:21:32 +0530

    Update birthdayCakeCandles.py

--- birthdayCakeCandles.py
+++ birthdayCakeCandles.py
@@ -1,38 +1,8 @@
 '''
-You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
-
-Example
-
-
-The maximum height candles are  units high. There are  of them, so return .
-
-Function Description
-
-Complete the function birthdayCakeCandles in the editor below.
-
-birthdayCakeCandles has the following parameter(s):
-
-int candles[n]: the candle heights
-Returns
-
-int: the number of candles that are tallest
-Input Format
-
-The first line contains a single integer, , the size of .
-The second line contains  space-separated integers, where each integer  describes the height of .
-
-Constraints
-
-Sample Input 0
-
-4
-3 2 1 3
-Sample Output 0
-
-2
-Explanation 0
-
-Candle heights are . The tallest candles are  units, and there are  of them.
+Problem 9. Birthday Cake Candles. You are in-charge of the cake for your niece's birthday and have decided the cake 
+will have one candle for each year of her total age. When she blows out the candles, she’ll only be able to blow out 
+the tallest ones. Your task is to find out how many candles she can successfully blow out.
+https://www.hackerrank.com/challenges/birthday-cake-candles/problem
 '''
 #!/bin/python3
 
