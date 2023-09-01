1c55c5214c7f5c7d55ed15d1c875edd8281f1a0d
ramvrav <33099104+ramvrav@users.noreply.github.com>
2023-09-01 16:27:07 +0530

    Update gradingStudents.py

--- gradingStudents.py
+++ gradingStudents.py
@@ -2,7 +2,7 @@
 Problem 11. Grading Students. Complete the function gradingStudents in the editor below. It should return an integer 
 array consisting of rounded grades. gradingStudents has the following parameter(s): grades: an array of integers 
 representing grades before rounding
-https://www.hackerrank.com/challenges/grading/submissions
+https://www.hackerrank.com/challenges/grading/problem
 '''
 #!/bin/python3
 
