cb230842994b75404b25762871ea8fb649976a61
ramvrav <33099104+ramvrav@users.noreply.github.com>
2023-09-04 15:59:27 +0530

    Update gradingStudents.py

--- gradingStudents.py
+++ gradingStudents.py
@@ -35,19 +35,12 @@ def gradingStudents(grades):
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     grades_count = int(input().strip())
-
     grades = []
-
     for _ in range(grades_count):
         grades_item = int(input().strip())
         grades.append(grades_item)
-
     result = gradingStudents(grades)
-
     fptr.write('\n'.join(map(str, result)))
     fptr.write('\n')
-
     fptr.close()
-
