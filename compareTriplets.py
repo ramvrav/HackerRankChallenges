cf31e153ca7db77eec300eac35953952d33f120b
ramvrav <33099104+ramvrav@users.noreply.github.com>
2023-09-04 15:48:26 +0530

    Update compareTriplets.py

--- compareTriplets.py
+++ compareTriplets.py
@@ -24,14 +24,9 @@ def solve(a, b):
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     a = list(map(int, input().rstrip().split()))
-
     b = list(map(int, input().rstrip().split()))
-
     result = solve(a, b)
-
     fptr.write(' '.join(map(str, result)))
     fptr.write('\n')
-
     fptr.close()
