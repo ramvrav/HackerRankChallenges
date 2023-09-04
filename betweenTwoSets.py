a15786a1cdda93ee9fdc9c0e207fe5d5b61f780a
ramvrav <33099104+ramvrav@users.noreply.github.com>
2023-09-04 15:43:12 +0530

    Update betweenTwoSets.py

--- betweenTwoSets.py
+++ betweenTwoSets.py
@@ -1,7 +1,7 @@
 '''
-There will be two arrays of integers. Determine all integers that satisfy the following two conditions:
-The elements of the first array are all factors of the integer being considered
-The integer being considered is a factor of all elements of the second array
+Problem 14. There will be two arrays of integers. Determine all integers that satisfy the following two conditions:
+--> The elements of the first array are all factors of the integer being considered
+--> The integer being considered is a factor of all elements of the second array
 These numbers are referred to as being between the two arrays. Determine how many such numbers exist.
 https://www.hackerrank.com/challenges/between-two-sets/problem
 '''
