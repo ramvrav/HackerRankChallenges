fc3921fe31be6b6b6ef40511f0e326b989ca6c79
ramvrav <33099104+ramvrav@users.noreply.github.com>
2023-09-04 15:52:00 +0530

    Update diagonalDiff.py

--- diagonalDiff.py
+++ diagonalDiff.py
@@ -20,16 +20,10 @@ def diagonalDifference(a):
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     n = int(input())
-
     a = []
-
     for _ in range(n):
         a.append(list(map(int, input().rstrip().split())))
-
     result = diagonalDifference(a)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
