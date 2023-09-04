ae672ba6e4cb1c8adb601450c1328f17cb519fd1
ramvrav <33099104+ramvrav@users.noreply.github.com>
2023-09-04 15:43:44 +0530

    Update birthdayCakeCandles.py

--- birthdayCakeCandles.py
+++ birthdayCakeCandles.py
@@ -20,13 +20,8 @@ def birthdayCakeCandles(ar):
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     ar_count = int(input())
-
     ar = list(map(int, input().rstrip().split()))
-
     result = birthdayCakeCandles(ar)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
