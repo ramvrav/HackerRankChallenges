cb2678bdfb23d22b68bc97ee98efd6d5dcb201ec
ramvrav <33099104+ramvrav@users.noreply.github.com>
2023-09-04 16:09:41 +0530

    Update repeatedString.py

--- repeatedString.py
+++ repeatedString.py
@@ -1,53 +1,7 @@
 '''
-There is a string, , of lowercase English letters that is repeated infinitely many times. Given an integer, , find and 
-print the number of letter a's in the first  letters of the infinite string.
-
-Example
-
-
-The substring we consider is , the first  characters of the infinite string. There are  occurrences of a in the 
-substring.
-
-Function Description
-
-Complete the repeatedString function in the editor below.
-
-repeatedString has the following parameter(s):
-
-s: a string to repeat
-n: the number of characters to consider
-Returns
-
-int: the frequency of a in the substring
-Input Format
-
-The first line contains a single string, .
-The second line contains an integer, .
-
-Constraints
-
-For  of the test cases, .
-Sample Input
-
-Sample Input 0
-
-aba
-10
-Sample Output 0
-
-7
-Explanation 0
-The first  letters of the infinite string are abaabaabaa. Because there are  a's, we return .
-
-Sample Input 1
-
-a
-1000000000000
-Sample Output 1
-
-1000000000000
-Explanation 1
-Because all of the first  letters of the infinite string are a, we return .
+Problem 26. There is a string, s, of lowercase English letters that is repeated infinitely many times. Given an 
+integer, n, find and print the number of letter a's in the first n letters of the infinite string.
+https://www.hackerrank.com/challenges/repeated-string/problem
 '''
 #!/bin/python3
 
@@ -79,13 +33,8 @@ def repeatedString(s, n):
     return answer
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     s = input()
-
     n = int(input().strip())
-
     result = repeatedString(s, n)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
